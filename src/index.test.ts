00dc4041755d5ec3b8940bc20d6bd4e0ad16c7b3
Mike Beasterfeld <mike.beastereld@gmail.com>
2022-11-16 04:19:33 +0000

    Specify the table in the select statement

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,16 @@
 import { SqlAbstract } from ".";
 
 describe("generate SQL", () =>  {
+    const sqlabstract = new SqlAbstract;
     it("should create a simple select statment", () => {
-        const sqlabstract = new SqlAbstract;
-        
         expect(sqlabstract.generateSQL()).toEqual("SELECT * FROM MYTABLE");
     });
 
-    it("should create a select statment with limited columns", () => {
-        const sqlabstract = new SqlAbstract;
-
+    it("should create a select statment with specific columns", () => {
         expect(sqlabstract.generateSQL({ columns: ["foo"] })).toEqual("SELECT foo FROM MYTABLE");
+    });
+
+    it("should create a select statement for any table", () => {
+        expect(sqlabstract.generateSQL({ table: "bar" })).toEqual("SELECT * FROM bar");
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export class SqlAbstract {
-    generateSQL(args?: Record<string, string[]>): string {
-        const columns = args && args.columns ? args.columns.join(",") : "*";
-        return `SELECT ${columns} FROM MYTABLE`;
+    generateSQL(args?: Record<string, string[] | string>): string {
+        const columns = args && Array.isArray(args.columns) && args.columns.length > 0 ? args.columns.join(",") : "*";
+        const table = args && args.table ? args.table : "MYTABLE";
+        return `SELECT ${columns} FROM ${table}`;
     }
 }
 
