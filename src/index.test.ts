d01156fad9251e4e05530e76b859e2c33b7d5c6b
Mike Beasterfeld <mike.beastereld@gmail.com>
2022-11-17 19:27:42 +0000

    prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,50 +1,86 @@
 import { SqlAbstract } from ".";
 
-describe("generate select SQL", () =>  {
-    const sqlabstract = new SqlAbstract;
-    it("should create a simple select statment", () => {
-        expect(sqlabstract.generateSQL()).toEqual("SELECT * FROM MYTABLE");
-    });
+describe("generate select SQL", () => {
+  const sqlabstract = new SqlAbstract();
+  it("should create a simple select statment", () => {
+    expect(sqlabstract.generateSQL()).toEqual("SELECT * FROM MYTABLE");
+  });
 
-    it("should create a select statment with specific columns", () => {
-        expect(sqlabstract.generateSQL({ columns: ["foo"] })).toEqual("SELECT foo FROM MYTABLE");
-    });
+  it("should create a select statment with specific columns", () => {
+    expect(sqlabstract.generateSQL({ columns: ["foo"] })).toEqual(
+      "SELECT foo FROM MYTABLE"
+    );
+  });
 
-    it("should create a select statement for any table", () => {
-        expect(sqlabstract.generateSQL({ table: "bar" })).toEqual("SELECT * FROM bar");
-    });
+  it("should create a select statement for any table", () => {
+    expect(sqlabstract.generateSQL({ table: "bar" })).toEqual(
+      "SELECT * FROM bar"
+    );
+  });
 
-    it("should create a select statment with a where clause", () => {
-        expect(sqlabstract.generateSQL({ where: ["foo", ">", "bar"]})).toEqual("SELECT * FROM MYTABLE WHERE foo > bar");
-    });
+  it("should create a select statment with a where clause", () => {
+    expect(sqlabstract.generateSQL({ where: ["foo", ">", "bar"] })).toEqual(
+      "SELECT * FROM MYTABLE WHERE foo > bar"
+    );
+  });
 
-    it("should create a select statement with a order by clause", () => {
-        expect(sqlabstract.generateSQL({ orderBy: [ "foo", "ASC" ]})).toEqual("SELECT * FROM MYTABLE ORDER BY foo ASC");        
-    });
+  it("should create a select statement with a order by clause", () => {
+    expect(sqlabstract.generateSQL({ orderBy: ["foo", "ASC"] })).toEqual(
+      "SELECT * FROM MYTABLE ORDER BY foo ASC"
+    );
+  });
 
-    it("should create a select with a join", () => {
-        expect(sqlabstract.generateSQL({ join: { type: "inner", table: "MYTABLE2", column: "mytable_id", baseTableColumn: "id" }})).toEqual("SELECT * FROM MYTABLE INNER JOIN MYTABLE2 ON MYTABLE.id = MYTABLE2.mytable_id");
-    });
+  it("should create a select with a join", () => {
+    expect(
+      sqlabstract.generateSQL({
+        join: {
+          type: "inner",
+          table: "MYTABLE2",
+          column: "mytable_id",
+          baseTableColumn: "id",
+        },
+      })
+    ).toEqual(
+      "SELECT * FROM MYTABLE INNER JOIN MYTABLE2 ON MYTABLE.id = MYTABLE2.mytable_id"
+    );
+  });
 });
 
-describe("generate insert SQL", () =>  {
-    const sqlabstract = new SqlAbstract;
-    it("should create a simple insert statement", () => {
-        expect(sqlabstract.generateSQL({ statementType: "insert", columns: ["columnFoo"], values: ["foo"]})).toEqual("INSERT INTO MYTABLE (columnFoo) VALUES ('foo')");
-    });
+describe("generate insert SQL", () => {
+  const sqlabstract = new SqlAbstract();
+  it("should create a simple insert statement", () => {
+    expect(
+      sqlabstract.generateSQL({
+        statementType: "insert",
+        columns: ["columnFoo"],
+        values: ["foo"],
+      })
+    ).toEqual("INSERT INTO MYTABLE (columnFoo) VALUES ('foo')");
+  });
 });
 
 describe("generate update SQL", () => {
-    const sqlabstract = new SqlAbstract;
-    it("should create a simple update statement", () => {
-        expect(sqlabstract.generateSQL({ statementType: "update", columns: ["columnFoo"], values: ["foo"], where: ["foo", ">", "bar"]})).toEqual("UPDATE MYTABLE SET columnFoo = 'foo' WHERE foo > bar");
-    });
+  const sqlabstract = new SqlAbstract();
+  it("should create a simple update statement", () => {
+    expect(
+      sqlabstract.generateSQL({
+        statementType: "update",
+        columns: ["columnFoo"],
+        values: ["foo"],
+        where: ["foo", ">", "bar"],
+      })
+    ).toEqual("UPDATE MYTABLE SET columnFoo = 'foo' WHERE foo > bar");
+  });
 });
 
 describe("generate delete SQL", () => {
-    const sqlabstract = new SqlAbstract;
-    it("should create a simple delete statement", () => {
-        expect(sqlabstract.generateSQL({ statementType: "delete", where: ["foo", ">", "bar"]})).toEqual("DELETE FROM MYTABLE WHERE foo > bar");
-    });
+  const sqlabstract = new SqlAbstract();
+  it("should create a simple delete statement", () => {
+    expect(
+      sqlabstract.generateSQL({
+        statementType: "delete",
+        where: ["foo", ">", "bar"],
+      })
+    ).toEqual("DELETE FROM MYTABLE WHERE foo > bar");
+  });
 });
-
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,62 @@
 export class SqlAbstract {
-    generateSQL(args?: GenerateSQLArgs): string {
-        const columns = args?.columns?.length ? args.columns.join(",") : "*";
-        const table = args?.table ? args.table : "MYTABLE";
-        const where = args?.where?.length ? " WHERE " + args.where.join(" ") : "";
-        const orderBy = args?.orderBy?.length ? " ORDER BY " + args.orderBy.join(" ") : "";
-        const values = args?.values?.length ? args.values.map(value => `'${value}'`).join(",") : "";
-        const setValues = args?.columns?.length ? args.columns.map((column, i) => {
-            if(args?.values && args.values[i]) {
-                console.log("column", column);
-                console.log("value", args.values[i]);
-                return [column, args.values[i]];
-            }
-        }) : [];
-        const join = args?.join ? ` ${args.join.type.toUpperCase()} JOIN ${args.join.table} ON ${table}.${args.join.baseTableColumn} = ${args.join.table}.${args.join.column}` : "";
+  generateSQL(args?: GenerateSQLArgs): string {
+    const columns = args?.columns?.length ? args.columns.join(",") : "*";
+    const table = args?.table ? args.table : "MYTABLE";
+    const where = args?.where?.length ? " WHERE " + args.where.join(" ") : "";
+    const orderBy = args?.orderBy?.length
+      ? " ORDER BY " + args.orderBy.join(" ")
+      : "";
+    const values = args?.values?.length
+      ? args.values.map((value) => `'${value}'`).join(",")
+      : "";
+    const setValues = args?.columns?.length
+      ? args.columns.map((column, i) => {
+          if (args?.values && args.values[i]) {
+            console.log("column", column);
+            console.log("value", args.values[i]);
+            return [column, args.values[i]];
+          }
+        })
+      : [];
+    const join = args?.join
+      ? ` ${args.join.type.toUpperCase()} JOIN ${args.join.table} ON ${table}.${
+          args.join.baseTableColumn
+        } = ${args.join.table}.${args.join.column}`
+      : "";
 
-        if (args?.statementType == "insert") {
-            return `INSERT INTO ${table} (${columns}) VALUES (${values})`;
-        }
-
-        if (args?.statementType == "update") {
-            const sets = setValues.map((set) => {
-                if(set) {
-                    return `${set[0]} = '${set[1]}'`;
-                }
-            });
-
-            return `UPDATE ${table} SET ${sets.join(",")}${where}`;
-        }
-
-        if (args?.statementType == "delete") {
-            return `DELETE FROM ${table}${where}`;
-        }
-
-        return `SELECT ${columns} FROM ${table}${join}${where}${orderBy}`;
+    if (args?.statementType == "insert") {
+      return `INSERT INTO ${table} (${columns}) VALUES (${values})`;
     }
+
+    if (args?.statementType == "update") {
+      const sets = setValues.map((set) => {
+        if (set) {
+          return `${set[0]} = '${set[1]}'`;
+        }
+      });
+
+      return `UPDATE ${table} SET ${sets.join(",")}${where}`;
+    }
+
+    if (args?.statementType == "delete") {
+      return `DELETE FROM ${table}${where}`;
+    }
+
+    return `SELECT ${columns} FROM ${table}${join}${where}${orderBy}`;
+  }
 }
 
 type GenerateSQLArgs = {
-    statementType?: "select" | "insert" | "update" | "delete",
-    columns?: string[],
-    values?: string[],
-    table?: string,
-    where?: string[],
-    orderBy?: string[],
-    join?: {
-        type: "inner",
-        table: string,
-        column: string,
-        baseTableColumn: string
-    }
-;}
-
+  statementType?: "select" | "insert" | "update" | "delete";
+  columns?: string[];
+  values?: string[];
+  table?: string;
+  where?: string[];
+  orderBy?: string[];
+  join?: {
+    type: "inner";
+    table: string;
+    column: string;
+    baseTableColumn: string;
+  };
+};
